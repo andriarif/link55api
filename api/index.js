cc80ff9f16a61cc3dda079a10b3b9cdc964804e1
andriarif <55911915+andriarif@users.noreply.github.com>
2026-04-27 21:03:23 +0700

    Refactor API calls and response handling

--- api/index.js
+++ api/index.js
@@ -5,75 +5,39 @@ res.setHeader("Access-Control-Allow-Methods", "GET");
 
 try{
 
-/* ================= GET ISSUE AKTIF ================= */
-
-const issueResp = await fetch(
-"https://newapi.55lottertttapi.com/api/webapi/GetGameIssue",
+const response = await fetch(
+"https://api.55fiveapi.com/api/webapi/GetNoaverageEmerdList",
 {
 method:"POST",
 
 headers:{
-"Content-Type":"application/json;charset=UTF-8"
-},
 
-body:JSON.stringify({
+"Content-Type":"application/json;charset=UTF-8",
 
-/* WINGO 1 MENIT */
+"Accept":"application/json, text/plain, */*",
 
-typeId:1,
+"Authorization":
+"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIxNzc3Mjk3Njk2IiwibmJmIjoiMTc3NzI5NzY5NiIsImV4cCI6IjE3NzcyOTk0OTYiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiI0LzI3LzIwMjYgOToxODoxNiBQTSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IkFjY2Vzc19Ub2tlbiIsIlVzZXJJZCI6IjEwOTY4NjMiLCJVc2VyTmFtZSI6IjYyODk1NDE0MzcwMTYyIiwiVXNlclBob3RvIjoiOSIsIk5pY2tOYW1lIjoiQW5kcmlwZWRpYSIsIkFtb3VudCI6IjMwLjQwIiwiSW50ZWdyYWwiOiIwIiwiTG9naW5NYXJrIjoiSDUiLCJMb2dpblRpbWUiOiI0LzI3LzIwMjYgODo0ODoxNiBQTSIsIkxvZ2luSVBBZGRyZXNzIjoiMTAzLjE2Ni45Mi4xOTgiLCJEYk51bWJlciI6IjAiLCJJc3ZhbGlkYXRvciI6IjAiLCJLZXlDb2RlIjoiMzAxNSIsIlRva2VuVHlwZSI6IkFjY2Vzc19Ub2tlbiIsIlBob25lVHlwZSI6IjEiLCJVc2VyVHlwZSI6IjAiLCJVc2VyTmFtZTIiOiJhbmRyaXBlZGlhOTBAZ21haWwuY29tIiwiaXNzIjoiand0SXNzdWVyIiwiYXVkIjoibG90dGVyeVRpY2tldCJ9.XWyroCj2s587J-QdApaGvyUQwxng8wLoNeG4EbbVWAQ",
 
-language:0,
+"Ar-Origin":"https://www.lopmiva.com"
 
-random:
-Math.random().toString(36).substring(2),
-
-signature:
-"5DB43C344C7381B72B5262FFB3572444",
-
-timestamp:
-Math.floor(Date.now()/1000)
-
-})
-
-}
-);
-
-const issueJson =
-await issueResp.json();
-
-const currentIssue =
-String(
-issueJson?.data?.issueNumber || ""
-).trim();
-
-/* ================= GET RESULT HISTORY ================= */
-
-const resultResp = await fetch(
-"https://newapi.55lottertttapi.com/api/webapi/GetNoaverageEmerdList",
-{
-method:"POST",
-
-headers:{
-"Content-Type":"application/json;charset=UTF-8"
 },
 
 body:JSON.stringify({
 
 pageSize:10,
-
 pageNo:1,
 
 /* WINGO 1 MENIT */
-
 typeId:1,
 
-language:0,
+language:1,
 
 random:
 Math.random().toString(36).substring(2),
 
 signature:
-"A6203E85132E5FE26B5F43DDF1ECDD07",
+"1F7A8D9E698F3155CB0E447679B1F5AD",
 
 timestamp:
 Math.floor(Date.now()/1000)
@@ -83,68 +47,48 @@ Math.floor(Date.now()/1000)
 }
 );
 
-const resultJson =
-await resultResp.json();
+const data =
+await response.json();
 
-const list =
-resultJson?.data?.list;
+const latest =
+data?.data?.list?.[0];
 
-/* ================= VALIDASI ================= */
-
-if(
-!list ||
-!Array.isArray(list) ||
-list.length < 1
-){
+if(!latest){
 
 return res.status(500).json({
 
 success:false,
-error:"Result tidak ditemukan"
+error:"Data tidak ditemukan"
 
 });
 
 }
 
-/* ================= RESULT FIX ================= */
-
-const latest =
-list.find(
-x =>
-String(x.issueNumber).trim()
-!== currentIssue
-) || list[0];
-
 const hasil =
 parseInt(latest.number);
-
-const resultIssue =
-String(
-latest.issueNumber || ""
-).trim();
-
-/* ================= RETURN ================= */
 
 return res.status(200).json({
 
 success:true,
 
-/* PERIODE BERJALAN */
-
-periode:currentIssue,
-
-/* RESULT TERAKHIR */
-
-resultIssue:resultIssue,
+periode:
+latest.issueNumber,
 
 hasil:hasil,
 
-number:latest.number,
+number:
+latest.number,
 
 status:
 hasil <= 4
 ? "SMALL"
 : "BIG",
+
+warna:
+latest.colour,
+
+premium:
+latest.premium,
 
 serverTime:
 Date.now()
@@ -156,10 +100,7 @@ Date.now()
 return res.status(500).json({
 
 success:false,
-
-error:"Server Error",
-
-detail:err.message
+error:err.toString()
 
 });
 
